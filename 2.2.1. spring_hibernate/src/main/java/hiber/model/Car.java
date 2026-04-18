549b9268fca6c2bb669a4545157216e11694a91e
Developavel <pa.mordovin@gmail.com>
2026-04-18 18:45:51 +0300

    Update Car.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -2,7 +2,14 @@ package hiber.model;
 
 public class Car {
 
-    String model;
-    int series;
+    private String model;
+    private int series;
 
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
 }
