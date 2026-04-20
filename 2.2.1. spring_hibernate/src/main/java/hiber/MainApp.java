fb9132ba778064d8b07d814e4a3c0faf586d4cb6
Developavel <pa.mordovin@gmail.com>
2026-04-20 11:22:37 +0300

    Update Main.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,10 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,18 +15,31 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+              new Car("BMW", 5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+              new Car("Audi", 6)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
+              new Car("Mercedes", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
+              new Car("Toyota", 7)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car Model = " + user.getCar().getModel());
+         System.out.println("Car Series = " + user.getCar().getSeries());
          System.out.println();
+      }
+
+      User user = userService.getUserByCarModelAndSeries("BMW", 5);
+      if (user != null) {
+         System.out.println("Владелец BMW 5: " + user.getFirstName() + " " + user.getLastName());
+      } else {
+         System.out.println("Не найден");
       }
 
       context.close();
